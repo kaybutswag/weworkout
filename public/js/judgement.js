4d618a9ab7e687252952ce9b50e174af799822d3
unknown <pmmilamiv@gmail.com>
2018-03-16 20:28:15 -0500

    added match modal and redesigned load icon

--- public/js/judgement.js
+++ public/js/judgement.js
@@ -14,6 +14,16 @@ function shuffleArray(arr){
 	return arr;
 }
 
+function openMatchModal (a, b, c, d) {
+	$('#matchOneImg').attr("style", "background-image: url("+a+")");
+	$('#matchOneName').text(b);
+	$('#matchTwoImg').attr("style", "background-image: url("+c+")");
+	$('#matchTwoName').text(d);
+  	$('.userCardImg').height($('.userCardImg').width());
+    var href = $('#matchLink').attr('href');
+    window.location.href = href;
+}
+ 
 function photoSlideshow() {
   if(counter > 0)
     $("#changingPic img").eq(counter - 1).removeClass("opaque");
@@ -113,6 +123,7 @@ function sendPreferences(userAge) {
 		myBigArray = matches;
 		currentProfile = 0;
 		$("#loader_judgement").addClass("display-none");
+		$("#loader_judgement").removeClass("fa-spin");
 		$("form").first().removeClass("display-none");
 		$(".exhausted-options").addClass("display-none");
 		showCard();
@@ -244,6 +255,7 @@ $(document).ready(function(){
 
 	$("#submitActivities").on("click", function(){
 		if(userAge > 0) {
+			$("#loader_judgement").attr("class", "fa-spin")
 			$("#loader_judgement").removeClass("display-none");
 			$("form").first().addClass("display-none");
 			$(".exhausted-options").addClass("display-none");
@@ -252,7 +264,7 @@ $(document).ready(function(){
 	});
 
 	$('.userCardImg').height($('.userCardImg').width());
-
+	
 	setInterval(photoSlideshow, 9000);
 });
 
